4c5b8dabafea1ccd44b0a9c55eefc41a8ba69ea8
pvt5r486 <pvt5r486@gmail.com>
2019-01-31 16:11:22 +0800

    九九乘法表完成

---
+++ src/components/multipcationTable.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="card" v-html="outPut">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'multipcationTable',
+  props: {
+    number: {
+      type: Number,
+      default: 1,
+    },
+  },
+  computed: {
+    outPut() {
+      const vm = this;
+      let part1 = '';
+      let part2 = '';
+      for (let i = 1; i < 10; i += 1) {
+        if (i <= 3) {
+          part1 = `${part1}<li>${vm.number} × ${i} ＝ ${vm.number * i}</li>`;
+        } else {
+          part2 = `${part2}<li>${vm.number} × ${i} ＝ ${vm.number * i}</li>`;
+        }
+      }
+      const HtmlStr = `
+        <div class="left_side">
+          <h2 class="card_title">${vm.number}</h2>
+          <ul class="calc">${part1}</ul>
+        </div>
+        <div class="right_side">
+          <ul class="calc">${part2}</ul>
+        </div>`;
+      return HtmlStr;
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+  .card {
+    width: 350px;
+    padding: 64px 40px 64px 40px;
+    color:#2EB738;
+    margin-bottom: 40px;
+    background: #FFFFFF;
+    font-family: 'Noto Sans TC', sans-serif;
+    display: flex;
+    overflow: hidden;
+    border-radius: 100px 0px 30px 0px;
+    box-shadow: 0px 3px 10px #D8D8D8;
+    &:nth-child(2),&:nth-child(5),&:nth-child(8){
+      margin-right: 30px;
+      margin-left: 30px;
+    }
+  }
+  .card_title{
+    font-size: 128px;
+    font-weight: bolder;
+    text-align: center;
+  }
+  .calc{
+    font-size: 24px;
+    li{
+      margin-bottom: 8px;
+      line-height: 33px;
+      &:last-child{
+        margin-bottom: 0px;
+      }
+    }
+  }
+  .right_side{
+    margin-left: auto;
+    margin-top: 4px;
+  }
+</style>
---
+++ src/views/Multiplication.vue
@@ -0,0 +1,140 @@
+<template>
+  <div class="warp">
+    <div class="container">
+      <div class="titleBlock">
+        <h1 class="title">九九乘法表</h1>
+        <span class="sub_title">MULTIPLICATION CHART</span>
+        <span class="cross-rightUp">×</span>
+        <span class="cross-rightDowm">×</span>
+        <span class="cross-leftUp">×</span>
+        <span class="cross-leftDowm">×</span>
+      </div>
+        <MultipcationTable v-for="item in blockNumberArray" :key="item.blockNumber"
+        :number="item"></MultipcationTable>
+    </div>
+    <footer>
+      <p class="lisence">Copyright © 2019 HexSchool. All rights reserved.</p>
+    </footer>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import MultipcationTable from '@/components/multipcationTable.vue';
+
+export default {
+  name: 'multiplication',
+  components: {
+    MultipcationTable,
+  },
+  data() {
+    return {
+      blockNumber: 8,
+    };
+  },
+  computed: {
+    blockNumberArray() {
+      const vm = this;
+      const arr = [];
+      for (let i = 1; i <= vm.blockNumber; i += 1) {
+        arr.push(i + 1);
+      }
+      return arr;
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+  .warp {
+    width: 1280px;
+    margin: 0 auto;
+    background: #F0F0F0;
+  }
+  .container {
+    padding-left: 85px;
+    padding-right: 85px;
+    padding-top: 80px;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+  .titleBlock{
+    width: 350px;
+    color:#2EB738;
+    position: relative;
+    margin-bottom: 40px;
+    padding-left: 35px;
+    padding-right: 35px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    &:before{
+      content: "";
+      position: absolute;
+      height: 1px;
+      background: #2EB738;
+      top: 12px;
+      right: 0;
+      left: 0;
+      margin-right: 35px;
+      margin-left: 35px;
+    }
+    &:after{
+      content: "";
+      position: absolute;
+      height: 1px;
+      background: #2EB738;
+      bottom: 12px;
+      right: 0;
+      left: 0;
+      margin-right: 35px;
+      margin-left: 35px;
+    }
+  }
+  .title{
+    font-size: 56px;
+    padding-top: 11px;
+    padding-bottom: 11px;
+    font-family: 'Noto Sans TC';
+  }
+  .sub_title{
+    display: block;
+    text-align: center;
+    font-size: 24px;
+    white-space: nowrap;
+    font-family: 'Noto Sans TC';
+  }
+  .cross-rightUp,.cross-rightDowm,.cross-leftUp,.cross-leftDowm{
+      position: absolute;
+      font-size: 24px;
+  }
+  .cross-rightUp{
+    top: 0;
+    right: 0;
+  }
+  .cross-rightDowm{
+    bottom: 0;
+    right: 0;
+  }
+  .cross-leftUp{
+    top: 0;
+    left: 0;
+  }
+  .cross-leftDowm{
+    bottom: 0;
+    left: 0;
+  }
+  footer {
+    background: #2EB738;
+    text-align: right;
+    .lisence{
+      padding-top: 8px;
+      padding-bottom: 8px;
+      padding-right: 85px;
+      font-size: 16px;
+      color: white;
+      font-family: 'Noto Sans TC', sans-serif;
+    }
+  }
+</style>
